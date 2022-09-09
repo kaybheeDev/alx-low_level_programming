36cf66ea3de6e7f7553e3b38eb55a3927c020508
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2022-09-08 20:33:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: use printif
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	printfr("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
