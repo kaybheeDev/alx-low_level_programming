e2e74cf4bb559f77d5dcf0d18c7aa2bfbb02795c
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2022-09-09 18:31:52 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  * Description: assigns random number to n each time n it's exhausted
@@ -14,11 +15,11 @@ int main(void)
 	n = rand() -RAND_MAX / 2'
 		if (n > 0)
 		{
-			printf("%d is positive\n" , n);
+			printf("%d is positive\n", n);
 		}
 		else if (n == 0)
 		{
-			printf("%d is zero\n" , n)
+			printf("%d is zero\n", n)
 		}
 		else if (n < 0)
 		{
