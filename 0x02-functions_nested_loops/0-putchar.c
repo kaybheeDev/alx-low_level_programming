b7112dc03341448a4d6b8e939dc3b1b845ea4243
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2022-09-13 09:23:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,11 @@
  * main - print putchar
  *Return: always 0
  */
+
 int main(void)
 {
        _putchar('_');
-       _putchar('P');
+       _putchar('p');
        _putchar('u');
        _putchar('t');
        _putchar('c');
