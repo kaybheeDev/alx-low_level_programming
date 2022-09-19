0982d81936301b16983cae77ba2f2d3b70eb2e3e
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2022-09-19 12:21:24 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - string length
+ * @s : parameter s
+ * Return: return the string length
+ */
+
+int _strlen(char *s)
+{
+int s_length;
+
+for (s_length = 0; s[s_length] != '\0'; s_length++)
+{
+}
+
+return (s_length);
+}
