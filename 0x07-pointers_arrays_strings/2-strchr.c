ecc2258bbb1a6be1afce0c8fd45cf05ae5a2496f
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2022-09-26 11:52:36 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - search character
+ * @s: string s
+ * @c: character to search
+ * Return: return at first doscovery
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+
+i= 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+return (s);
+i++;
+}
+
+if (s[i] == c)
+return (s);
+
+return (0);
+}
