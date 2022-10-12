3fa4905a313ff6f309990bc0b9c2b2d5e6d480e0
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2022-10-12 16:26:42 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,7 @@
  * @size: size of the array
  * @action: pointer to function used
  */
-void array_iterator(int *array  size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
 
