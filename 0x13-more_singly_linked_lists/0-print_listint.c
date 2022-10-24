7e68f2f1a43feec573e1b637ba412e505fa2778f
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2022-10-24 12:04:27 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * prints_istint - prints all the elements of a linked list
+ * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
